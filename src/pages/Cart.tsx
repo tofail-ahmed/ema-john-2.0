8654a3860949fcb562141c7048f463b53ef1361f
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-02-24 00:11:56 +0600

    cart data retrieved from store by using useAppSelector

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,18 +1,20 @@
 import CartDetails from "../components/CartDetails";
 import OrderSummary from "../components/OrderSummary";
 import getAllProducts from "../data/products";
+import { useAppSelector } from "../redux/hook";
 
 const Cart = () => {
-  const products = getAllProducts();
+  // const products = getAllProducts();
+  const products=useAppSelector((store)=>store.cart.products)
   return (
     <div className="container mt-10 mx-auto">
       <div className="flex lg:flex-row flex-col-reverse justify-center lg:space-x-40 ">
         <div className="space-y-5 lg:mt-0 mt-5">
-          <CartDetails product={products[0]} />
-          <CartDetails product={products[0]} />
-          <CartDetails product={products[0]} />
-          <CartDetails product={products[0]} />
-          <CartDetails product={products[0]} />
+          {
+            products.map((product)=>(
+              <CartDetails key={product.id} product={product}/>
+            ))
+          }
         </div>
         <OrderSummary />
       </div>
